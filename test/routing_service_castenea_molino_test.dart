4269d30311020a0080f3a58700038f8def4de759
jesus.diazsantos <jesus.diazsantos@sngular.com>
2024-05-23 14:07:06 +0200

    refactor test

--- test/routing_service_castenea_molino_test.dart
+++ test/routing_service_castenea_molino_test.dart
@@ -1,77 +1,22 @@
-import 'package:collection/collection.dart';
-import 'package:routing_service/enums/node_type_enum.dart';
 import 'package:routing_service/enums/run_type_enum.dart';
-import 'package:routing_service/model/node.dart';
-import 'package:routing_service/model/user_option.dart';
-import 'package:routing_service/routing_service.dart' as routing_service;
-import 'package:routing_service/utils/geojson_utils.dart';
 import 'package:test/test.dart';
+
+import 'calculate_route_util_test.dart';
 
 void main() {
   test('calculateRoute Castanea to molino with user level novice', () async {
-    await testCalculateRoute(RunType.novice);
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.novice);
   });
 
   test('calculateRoute Castanea to molino with user level easy', () async {
-    await testCalculateRoute(RunType.easy);
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.easy);
   });
 
-  test('calculateRoute Castanea to molino with user level intermediate',
-      () async {
-    await testCalculateRoute(RunType.intermediate);
+  test('calculateRoute Castanea to molino with user level intermediate', () async {
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.intermediate);
   });
 
   test('calculateRoute Castanea to molino with user level advanced', () async {
-    await testCalculateRoute(RunType.advanced);
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.advanced);
   });
-}
-
-Future<void> testCalculateRoute(RunType userLevel) async {
-  UserOption userOption = UserOption(level: userLevel);
-  bool exceptionThrown = false;
-
-  try {
-    List<double> castanesaCoordinates = [42.5523, 0.6062, 2025.8];
-    List<double> molinoCoordinates = [42.586867299999994, 0.5400737, 1506.83];
-
-    List<Node> nodes = await routing_service.calculateRouteReturnNode(
-        userOption, castanesaCoordinates, molinoCoordinates);
-
-    var invalidRunNodes = nodes
-        .where((node) =>
-            node.nodeType == NodeType.run &&
-            node.runTypes
-                .none((runType) => runType.index <= userOption.level.index))
-        .toList();
-
-    //printNodes(invalidRunNodes, userOption);
-    //printGeoJson(nodes);
-
-    expect(invalidRunNodes.isEmpty, isTrue,
-        reason: 'The route contains a track higher level than the user.');
-  } catch (e) {
-    print('Exception caught: $e');
-    exceptionThrown = true;
-  }
-
-  if (exceptionThrown) {
-    expect(exceptionThrown, isTrue,
-        reason:
-            'There should be a route available within the user\'s skill level');
-  }
-}
-
-void printGeoJson(List<Node> nodes) {
-  final geoJson = GeoJsonUtils.pathToGeoJson(nodes);
-  print('GeoJSON:');
-  print(geoJson);
-}
-
-void printNodes(List<Node> nodes, UserOption userOption) {
-  for (var node in nodes) {
-    if (node.nodeType == NodeType.run) {
-      print(
-          'Nodo: ${node.names} ${node.latitude} ${node.longitude} ${node.altitude}, RunType: ${node.runTypes}, Nivel de usuario: ${userOption.level}');
-    }
-  }
 }
--- test/routing_service_molino_castenea_test.dart
+++ test/routing_service_molino_castenea_test.dart
@@ -1,84 +1,30 @@
-import 'package:collection/collection.dart';
-import 'package:routing_service/enums/node_type_enum.dart';
 import 'package:routing_service/enums/run_type_enum.dart';
-import 'package:routing_service/model/node.dart';
-import 'package:routing_service/model/user_option.dart';
-import 'package:routing_service/routing_service.dart' as routing_service;
-import 'package:routing_service/utils/geojson_utils.dart';
 import 'package:test/test.dart';
+
+import 'calculate_route_util_test.dart';
 
 void main() {
   test('calculateRoute Molino to Castanea with user level novice', () async {
-    await testCalculateRoute(UserOption(level: RunType.novice));
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.novice);
   });
 
   test('calculateRoute Molino to Castanea with user level easy', () async {
-    await testCalculateRoute(UserOption(level: RunType.easy));
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.easy);
   });
 
-  test('calculateRoute Molino to Castanea with user level intermediate',
-      () async {
-    await testCalculateRoute(UserOption(level: RunType.intermediate));
+  test('calculateRoute Molino to Castanea with user level intermediate', () async {
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.intermediate);
   });
 
   test('calculateRoute Molino to Castanea with user level advanced', () async {
-    await testCalculateRoute(UserOption(level: RunType.advanced));
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.advanced);
   });
 
   test('calculateRoute Molino to Castanea with user level expert', () async {
-    await testCalculateRoute(UserOption(level: RunType.expert));
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.expert);
   });
 
   test('calculateRoute Molino to Castanea with user level freeride', () async {
-    await testCalculateRoute(UserOption(level: RunType.freeride));
+    await CalculateRouteUtilTest.testCalculateRoute(RunType.freeride);
   });
-}
-
-Future<void> testCalculateRoute(UserOption userOption) async {
-  bool exceptionThrown = false;
-
-  try {
-    List<double> castanesaCoordinates = [42.5523, 0.6062, 2025.8];
-    List<double> molinoCoordinates = [42.586867299999994, 0.5400737, 1506.83];
-
-    List<Node> nodes = await routing_service.calculateRouteReturnNode(
-        userOption, molinoCoordinates, castanesaCoordinates);
-
-    var invalidRunNodes = nodes
-        .where((node) =>
-            node.nodeType == NodeType.run &&
-            node.runTypes
-                .none((runType) => runType.index <= userOption.level.index))
-        .toList();
-
-    //printNodes(invalidRunNodes, userOption);
-    //printGeoJson(nodes);
-
-    expect(invalidRunNodes.isEmpty, isTrue,
-        reason: 'The route contains a track higher level than the user.');
-  } catch (e) {
-    print('Exception caught: $e');
-    exceptionThrown = true;
-  }
-
-  if (exceptionThrown) {
-    expect(exceptionThrown, isTrue,
-        reason:
-            'There should be a route available within the user\'s skill level');
-  }
-}
-
-void printGeoJson(List<Node> nodes) {
-  final geoJson = GeoJsonUtils.pathToGeoJson(nodes);
-  print('GeoJSON:');
-  print(geoJson);
-}
-
-void printNodes(List<Node> nodes, UserOption userOption) {
-  for (var node in nodes) {
-    if (node.nodeType == NodeType.run) {
-      print(
-          'Nodo: ${node.names} ${node.latitude} ${node.longitude} ${node.altitude}, RunType: ${node.runTypes}, Nivel de usuario: ${userOption.level}');
-    }
-  }
 }
