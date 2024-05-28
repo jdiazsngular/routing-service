fcc12cae75c7e04b1a74baacfcc11186142894a8
Pablo Cabo <pablo.cabo@sngular.com>
2024-05-28 16:45:24 +0200

    Updating tests

--- test/routing_service_castenea_molino_test.dart
+++ test/routing_service_castenea_molino_test.dart
@@ -5,6 +5,8 @@ import 'package:routing_service/routing_service.dart' as routing_service;
 import 'package:routing_service/utils/geojson_utils.dart';
 import 'package:test/test.dart';
 
+import 'calculate_route_util_test.dart';
+
 void main() {
   group("Calculate Route from", () {
     test('Castanea to molino with user level novice', () async {
@@ -27,12 +29,13 @@ void main() {
       List<Step> steps = await routing_service.calculateRouteReturnNode(
           userOption, Fixture.castanesaBottomLiftCoordinates, Fixture.molinoBottomLiftCoordinates);
 
-      print(GeoJsonUtils.pathToGeoJson(steps));
       var invalidRunSteps = routing_service.getInvalidRunSteps(steps, userOption.level);
       expect(invalidRunSteps, hasLength(1));
       expect(steps, containsCoordinates(Fixture.basibeNouFontsRunCoordinates));
       expect(steps, containsCoordinates(Fixture.basibeRunCoordinates));
       expect(steps, containsCoordinates(Fixture.batisiellesTopLiftCoordinates));
+      expect(steps, containsCoordinates(Fixture.barrancoRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.lesPlanesRunCoordinates));
     });
 
     test('calculateRoute Castanea to molino with user level intermediate', () async {
@@ -41,13 +44,15 @@ void main() {
       List<Step> steps = await routing_service.calculateRouteReturnNode(
           userOption, Fixture.castanesaBottomLiftCoordinates, Fixture.molinoBottomLiftCoordinates);
 
-  print(GeoJsonUtils.pathToGeoJson(steps));
       var invalidRunSteps = routing_service.getInvalidRunSteps(steps, userOption.level);
       expect(invalidRunSteps, hasLength(0));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.basibeTopCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.ampriuBottomCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.sarrauTopCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.molinoTopCoordinates.toString())));
+      expect(steps, containsCoordinates(Fixture.castanesaBottomLiftCoordinates));
+      expect(steps, containsCoordinates(Fixture.basibeNouFontsRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.laFetiellaRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.batisiellesTopLiftCoordinates));
+      expect(steps, containsCoordinates(Fixture.barrancoRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.fontanalsRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.molinoBottomLiftCoordinates));
     });
 
     test('calculateRoute Castanea to molino with user level advanced', () async {
@@ -56,12 +61,13 @@ void main() {
       List<Step> steps = await routing_service.calculateRouteReturnNode(
           userOption, Fixture.castanesaBottomLiftCoordinates, Fixture.molinoBottomLiftCoordinates);
 
+      print(GeoJsonUtils.pathToGeoJson(steps));
+
       var invalidRunSteps = routing_service.getInvalidRunSteps(steps, userOption.level);
       expect(invalidRunSteps, hasLength(0));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.basibeTopLiftCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.ampriuBottomLiftCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.sarrauTopLiftCoordinates.toString())));
-      // expect(steps, contains(predicate<Step>((s) => s.node.toString() == Fixture.molinoTopLiftCoordinates.toString())));
+      expect(steps, containsCoordinates(Fixture.basibeNouFontsRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.pasoLobinoRunCoordinates));
+      expect(steps, containsCoordinates(Fixture.ampriuBottomLiftCoordinates));
     });
   });
 
@@ -89,6 +95,12 @@ class Fixture {
   static final List<double> molinoBottomLiftCoordinates = [42.586867299999994, 0.5400737, 1506.83];
   static final List<double> castanesaBottomLiftCoordinates = [42.5523, 0.6062, 2025.8];
   static final List<double> batisiellesTopLiftCoordinates = [42.554961899999995, 0.5531225, 2251.9500000000003];
+
   static final List<double> basibeNouFontsRunCoordinates = [42.55187939999999, 0.5903671999999996, 2324.7];
   static final List<double> basibeRunCoordinates = [42.5536956, 0.5813788999999998, 2165.59];
+  static final List<double> barrancoRunCoordinates = [42.558192300000016, 0.5486167, 2183.74];
+  static final List<double> lesPlanesRunCoordinates = [42.574269099999995, 0.5414334, 1799.3];
+  static final List<double> laFetiellaRunCoordinates = [42.55398639999999, 0.5867157, 2187.54];
+  static final List<double> fontanalsRunCoordinates = [42.57232560000001, 0.54432, 1882.8];
+  static final List<double> pasoLobinoRunCoordinates = [42.554481999999986, 0.5865514999999998, 2183.84];
 }
